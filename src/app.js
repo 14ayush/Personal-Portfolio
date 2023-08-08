0ce9581ebea0a400542d2b123cbb78a96f5c97a2
AYUSH SHARMA <aayushparashar984@gmail.com>
2023-08-09 01:26:15 +0530

    This is my Personal Portfolio
    I am currently working on my Portfolio using React Js and tailwind css

---
+++ src/app.js
@@ -0,0 +1,12 @@
+
+import nav from './components/nav';
+
+function app() {
+  return (
+   <div>
+    <nav />
+   </div>
+  );
+}
+
+export default app;
---
+++ src/components/nav.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {FaBars,FaTimes} from 'react-icons/fa';
+
+const nav = () => {
+  return (
+    <div>
+        <FaBars />
+    </div>
+  );
+}
+
+export default nav;
